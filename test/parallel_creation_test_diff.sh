f9579463d84f59bc4d2c151882943bc237877eb1
Alex Reynolds <alexpreynolds@gmail.com>
2015-10-26 00:48:49 -0700

    updated parallel tests for larger inputs

--- test/parallel_creation_test_diff.sh
+++ test/parallel_creation_test_diff.sh
@@ -3,11 +3,11 @@
 PWD=`pwd`
 
 # test difference
-xxd $PWD/vec_test4.sqr.bs > $PWD/vec_test4.sqr.bs.hex
-cat $PWD/vec_test4.sqr.bs1.rbs.blocks/0* | xxd > $PWD/vec_test4.sqr.bs1.rbs.blocks/vec_test4.sqr.bs.hex
-diff -s $PWD/vec_test4.sqr.bs.hex $PWD/vec_test4.sqr.bs1.rbs.blocks/vec_test4.sqr.bs.hex
-cat $PWD/vec_test4.sqr.bs1.rbs.qsub.blocks/0* | xxd > $PWD/vec_test4.sqr.bs1.rbs.qsub.blocks/vec_test4.sqr.bs.hex
-diff -s $PWD/vec_test4.sqr.bs.hex $PWD/vec_test4.sqr.bs1.rbs.qsub.blocks/vec_test4.sqr.bs.hex
-rm -f $PWD/vec_test4.sqr.bs.hex
-rm -f $PWD/vec_test4.sqr.bs1.rbs.blocks/vec_test4.sqr.bs.hex
-rm -f $PWD/vec_test4.sqr.bs1.rbs.qsub.blocks/vec_test4.sqr.bs.hex
+xxd $PWD/vec_test1000.sqr.bs > $PWD/vec_test1000.sqr.bs.hex
+cat $PWD/vec_test1000.sqr.bs250.rbs.blocks/0* | xxd > $PWD/vec_test1000.sqr.bs250.rbs.blocks/vec_test1000.sqr.bs.hex
+diff -s $PWD/vec_test1000.sqr.bs.hex $PWD/vec_test1000.sqr.bs250.rbs.blocks/vec_test1000.sqr.bs.hex
+cat $PWD/vec_test1000.sqr.bs250.rbs.qsub.blocks/0* | xxd > $PWD/vec_test1000.sqr.bs250.rbs.qsub.blocks/vec_test1000.sqr.bs.hex
+diff -s $PWD/vec_test1000.sqr.bs.hex $PWD/vec_test1000.sqr.bs250.rbs.qsub.blocks/vec_test1000.sqr.bs.hex
+rm -f $PWD/vec_test1000.sqr.bs.hex
+rm -f $PWD/vec_test1000.sqr.bs250.rbs.blocks/vec_test1000.sqr.bs.hex
+rm -f $PWD/vec_test1000.sqr.bs250.rbs.qsub.blocks/vec_test1000.sqr.bs.hex
