e8df8d2be7818c5a3865e14f5c216c123bf41344
Alex Reynolds <alexpreynolds@gmail.com>
2015-10-27 11:07:44 -0700

    added script to generate row enumeration of index input BED file

---
+++ test/enumerate_index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+# input:  BED3 or greater
+# output: BED4, fourth column is zero-indexed row index ("row enumeration")
+
+awk '{ print $1"\t"$2"\t"$3"\t"(NR-1); }' $1 > "${1%.*}.enum.bed"
